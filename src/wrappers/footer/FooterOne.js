9d9d9aa0181bbebb0c850b915d81067470a2445d
Mansab Mir <mansabmirsde@gmail.com>
2024-09-20 21:37:55 +0530

    footer codechip added

--- src/wrappers/footer/FooterOne.js
+++ src/wrappers/footer/FooterOne.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import FooterCopyright from "../../components/footer/FooterCopyright";
 import FooterNewsletter from "../../components/footer/FooterNewsletter";
 
-
 const FooterOne = ({
   backgroundColorClass,
   spaceTopClass,
@@ -13,12 +12,22 @@ const FooterOne = ({
   spaceRightClass,
   containerClass,
   extraFooterClass,
-  sideMenu
+  sideMenu,
 }) => {
   return (
-    <footer className={clsx("footer-area", backgroundColorClass, spaceTopClass, spaceBottomClass, extraFooterClass, spaceLeftClass, spaceRightClass )}>
+    <footer
+      className={clsx(
+        "footer-area",
+        backgroundColorClass,
+        spaceTopClass,
+        spaceBottomClass,
+        extraFooterClass,
+        spaceLeftClass,
+        spaceRightClass
+      )}
+    >
       <div className={`${containerClass ? containerClass : "container"}`}>
-        <div className="row">
+        <div className='row'>
           <div
             className={`${
               sideMenu ? "col-xl-2 col-sm-4" : "col-lg-2 col-sm-4"
@@ -26,8 +35,8 @@ const FooterOne = ({
           >
             {/* footer copyright */}
             <FooterCopyright
-              footerLogo="/assets/img/logo/logo.png"
-              spaceBottomClass="mb-30"
+              footerLogo='/assets/img/logo/logo.png'
+              spaceBottomClass='mb-30'
             />
           </div>
           <div
@@ -35,11 +44,11 @@ const FooterOne = ({
               sideMenu ? "col-xl-2 col-sm-4" : "col-lg-2 col-sm-4"
             }`}
           >
-            <div className="footer-widget mb-30 ml-30">
-              <div className="footer-title">
+            <div className='footer-widget mb-30 ml-30'>
+              <div className='footer-title'>
                 <h3>ABOUT US</h3>
               </div>
-              <div className="footer-list">
+              <div className='footer-list'>
                 <ul>
                   <li>
                     <Link to={process.env.PUBLIC_URL + "/about"}>About us</Link>
@@ -75,10 +84,10 @@ const FooterOne = ({
                   : "footer-widget mb-30 ml-50"
               }`}
             >
-              <div className="footer-title">
+              <div className='footer-title'>
                 <h3>USEFUL LINKS</h3>
               </div>
-              <div className="footer-list">
+              <div className='footer-list'>
                 <ul>
                   <li>
                     <Link to={process.env.PUBLIC_URL + "#/"}>Returns</Link>
@@ -110,43 +119,43 @@ const FooterOne = ({
                   : "footer-widget mb-30 ml-75"
               }`}
             >
-              <div className="footer-title">
+              <div className='footer-title'>
                 <h3>FOLLOW US</h3>
               </div>
-              <div className="footer-list">
+              <div className='footer-list'>
                 <ul>
                   <li>
                     <a
-                      href="//www.facebook.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
+                      href='//www.facebook.com'
+                      target='_blank'
+                      rel='noopener noreferrer'
                     >
                       Facebook
                     </a>
                   </li>
                   <li>
                     <a
-                      href="//www.twitter.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
+                      href='//www.twitter.com'
+                      target='_blank'
+                      rel='noopener noreferrer'
                     >
                       Twitter
                     </a>
                   </li>
                   <li>
                     <a
-                      href="//www.instagram.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
+                      href='//www.instagram.com'
+                      target='_blank'
+                      rel='noopener noreferrer'
                     >
                       Instagram
                     </a>
                   </li>
                   <li>
                     <a
-                      href="//www.youtube.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
+                      href='//www.youtube.com'
+                      target='_blank'
+                      rel='noopener noreferrer'
                     >
                       Youtube
                     </a>
@@ -162,13 +171,21 @@ const FooterOne = ({
           >
             {/* footer newsletter */}
             <FooterNewsletter
-              spaceBottomClass="mb-30"
-              spaceLeftClass="ml-70"
+              spaceBottomClass='mb-30'
+              spaceLeftClass='ml-70'
               sideMenu={sideMenu}
             />
           </div>
         </div>
       </div>
+      <div></div>
+      <p style={{ fontSize: "12px", textAlign: "center" }}>
+        Design & Developed By
+        <a href='https://www.codechip.in' target='_blank' rel='noreferrer'>
+          {" "}
+          Codechip Studios
+        </a>
+      </p>
     </footer>
   );
 };
@@ -181,7 +198,7 @@ FooterOne.propTypes = {
   spaceBottomClass: PropTypes.string,
   spaceTopClass: PropTypes.string,
   spaceLeftClass: PropTypes.string,
-  spaceRightClass: PropTypes.string
+  spaceRightClass: PropTypes.string,
 };
 
 export default FooterOne;
